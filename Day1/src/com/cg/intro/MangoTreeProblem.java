65e7c626811f49b164cbc9f68f4f3e081c299ede
HP <HP@LAPTOP-H14LCGKH>
2022-07-24 11:56:40 +0530

    first commit

---
+++ Day1/src/com/cg/intro/MangoTreeProblem.java
@@ -0,0 +1,22 @@
+package com.cg.intro;
+
+public class MangoTreeProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r=5;
+		int c=5;
+		int n=20;
+		
+		if(r*2==n || r*(c-1)==n)
+		{
+			System.out.println("It is a Mango Tree");
+		}
+		else
+		{
+			System.out.println("It is not a Mango Tree");
+		}
+
+	}
+
+}
