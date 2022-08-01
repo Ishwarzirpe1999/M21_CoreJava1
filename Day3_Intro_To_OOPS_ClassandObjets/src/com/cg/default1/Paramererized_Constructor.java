ab6423f5ed754d2c826c700729567bb77cb04b14
HP <HP@LAPTOP-H14LCGKH>
2022-08-01 09:49:23 +0530

    first commit

---
+++ Day3_Intro_To_OOPS_ClassandObjets/src/com/cg/default1/Paramererized_Constructor.java
@@ -0,0 +1,19 @@
+package com.cg.default1;
+
+class B
+{
+	B(int a)
+	{
+		System.out.println("The value of a is :"+a);
+	}
+}
+
+public class Paramererized_Constructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		B obj = new B(25);
+     
+	}
+
+}
