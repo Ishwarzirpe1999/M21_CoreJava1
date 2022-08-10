3a06e70bf66ecb981d43c4da2cd65b78abdb04ee
HP <HP@LAPTOP-H14LCGKH>
2022-08-10 18:33:54 +0530

    first commit

---
+++ Day13_Collection1.1/src/com/cg/list/ArrayListExample.java
@@ -0,0 +1,18 @@
+package com.cg.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		//declare the arraylist from collection class
+		List<Integer>obj=new ArrayList<>();
+		obj.add(11);
+		obj.add(13);
+		obj.add(15);
+		System.out.println("ArrayList elements are "+obj);
+
+	}
+
+}
---
+++ Day13_Collection1.1/src/com/cg/list/ExampleArrayList.java
@@ -0,0 +1,19 @@
+package com.cg.list;
+
+import java.util.ArrayList;
+
+public class ExampleArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList a = new ArrayList();
+		for(int i=0;i<3;i++)
+		{
+			a.add(i);
+		}
+		System.out.println(a);
+		
+
+	}
+
+}
